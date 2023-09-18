59c72f4cdb6f39b670099f789cc64a0e443527a5
Peter <houpengg@outlook.com>
2023-09-17 22:57:54 -0400

    upgrade libs and new full version example

--- examples/full/next.config.js
+++ examples/full/next.config.js
@@ -1,18 +1,41 @@
+/** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
+    enabled: process.env.ANALYZE === 'true'
 })
 
 module.exports = withBundleAnalyzer({
-  staticPageGenerationTimeout: 300,
-  images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      's3.us-west-2.amazonaws.com'
-    ],
-    formats: ['image/avif', 'image/webp']
-  }
+    staticPageGenerationTimeout: 300,
+    images: {
+        domains: [
+        'www.notion.so',
+        'notion.so',
+        'images.unsplash.com',
+        'pbs.twimg.com',
+        's3.us-west-2.amazonaws.com'
+        ],
+        formats: ['image/avif', 'image/webp'],
+    },
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+        serverActions: true,
+    },
+    webpack: (config, { webpack }) => {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        config.externals.push({
+            sharp: "commonjs sharp",
+            canvas: "commonjs canvas",
+        })
+        config.plugins.push(
+            new webpack.ProvidePlugin({
+                Buffer: ["buffer", "Buffer"],
+                process: "process/browser",
+            })
+        )
+        return config
+    },
 })
